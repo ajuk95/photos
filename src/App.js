c83e6f811ab5bf8eff456d6aef4c60e58f8b0c8a
Aju <aju0ajmal0k@gmail.com>
2021-03-09 19:06:49 +0530

    created photos having infinite scroll

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Photos from "./Photos.js";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="header">Photos</h1>
+      <Photos/>
+    </div>
+  );
+}
+
+export default App;  
---
+++ src/Photos.js
@@ -0,0 +1,59 @@
+import React from "react";
+import axios from "axios";
+import InfiniteScroll from "react-infinite-scroll-component"; 
+import "./photos.css"; 
+
+const GetPhoto = ({ url, key }) => (
+    <div className="photo" key={key} >
+        <img src={url} alt=""/>
+    </div>
+  );
+
+export default function Photos (){
+
+    const [photos, setPhotos] = React.useState([]);
+    const [loaded, setLoaded] = React.useState(false);
+
+    
+    React.useEffect(() => {
+        fetchImages();
+    }, []);
+    
+    const fetchImages = (count = 15) => {
+        const apiRoot = "https://api.unsplash.com";
+        const accessKey = "nP3ufTkXeJvnKZUmFMNKUeFF-ybpH7vn_pG9PSLjvJs";
+        axios
+        .get(`${apiRoot}/photos/random?client_id=${accessKey}&count=${count}`)
+        .then (response => {
+            setPhotos([...photos, ...response.data]);
+            setLoaded(true); 
+        });
+    };
+
+    return (
+        <div className="main">
+              <InfiniteScroll
+                dataLength={photos}
+                next={() => fetchImages(10)}  
+                hasMore={true}
+                loader={
+                  <img
+                    src="https://th.bing.com/th/id/OIP.aNr2Az1aHm9Aqi0V-SVV-QHaFj?w=212&h=180&c=7&o=5&dpr=1.2&pid=1.7"
+                    alt="loading"
+                  />
+                }  
+              >
+                <div className="allPhotos">
+                  {loaded
+                    ? photos.map((photo, index) => (
+                        <GetPhoto 
+                          url={photo.urls.thumb} 
+                          key={index}
+                        />
+                      ))
+                    : ""}
+                </div>
+              </InfiniteScroll>
+        </div>
+      );
+};
